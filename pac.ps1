97349c54a92a0e5c703b8ca12a1b929f8980d02e
Utkan Gezer <utkangezer@gmail.com>
2019-06-09 17:54:03 +0300

    Improved pac to prompt creation of an enviornment if absent.

--- pac.ps1
+++ pac.ps1
@@ -1,2 +1,18 @@
-.\env\Scripts\activate
+if (Test-Path 'env/Scripts/activate') {
+	.\env\Scripts\activate
+}
+else {
+	$message = 'Virtual Environment Absent';
+	$question = 'Would you like to create one first?';
+
+	$choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription];
+	$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'));
+	$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'));
+
+	$decision = $Host.UI.PromptForChoice($message, $question, $choices, 0);
+	if ($decision -eq 0) {
+		python -m venv env
+		pac
+	}
+}
 
