465a2a60c89aca09efa7df98172a672d94660d96
Utkan Gezer <utkangezer@gmail.com>
2019-06-15 21:11:44 +0300

    gs now lists the non-git directories at its end.
    Only the top-most directories with no sub-git-directories are listed.

--- gs.ps1
+++ gs.ps1
@@ -1,14 +1,38 @@
-function CheckPath($path) {
+function CheckPath($path, [ref]$gitonbranch, $nongits) {
 	if (Test-Path (Join-Path $path .git)) {
 		$status = git -C $path status -s
 		$stash = git -C $path stash list
 		if (($status -ne $null) -or ($stash -ne $null)) {
 			Write-Output "> $path" $status $stash ""
 		}
+		$gitonbranch.value = $true
+	}
+	else {
+		$exgit = $false
+		$ngs = New-Object Collections.Generic.List[System.Object]
+		ForEach ($item in (Get-ChildItem -Path $path -Directory)) {
+			$gob = $false
+			CheckPath ($item.FullName) ([ref]$gob) $nongits
+			if ($gob) {
+				$exgit = $true
+			}
+			else {
+				$ngs.Add($item.Fullname)
+			}
+		}
+
+		if ($exgit) {
+			$gitonbranch.value = $true
+			$nongits.AddRange($ngs)
+		}
 	}
 }
 
-CheckPath(pwd)
-ForEach ($item in (Get-ChildItem -Recurse -Directory)) {
-	CheckPath($item.FullName)
+$gitonbranch = $false
+$nongits = New-Object Collections.Generic.List[System.Object]
+CheckPath (pwd) ([ref]$gitonbranch) $nongits
+
+ForEach ($ng in $nongits) {
+	Write-Output "- $ng"
 }
+
