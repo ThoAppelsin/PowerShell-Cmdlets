02ea1741d29cb68856c2fdbf4ef16376b4d35479
Utkan Gezer <utkangezer@gmail.com>
2019-06-17 01:23:06 +0300

    Added newlines between parts of syn.

--- syn.ps1
+++ syn.ps1
@@ -1,11 +1,14 @@
 Write-Host '== Vim Sync =='
 vsyn
 
+Write-Host ''
 Write-Host '== Cmdlets Sync =='
 csyn
 
+Write-Host ''
 Write-Host '== PowerShell Sync =='
 psyn
 
+Write-Host ''
 Write-Host '== Subl3 Sync =='
 ssyn
